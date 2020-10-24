a38ca33a9f99919e30d8d787f67477333172b91a
Aristóteles Machado <ario2025@users.noreply.github.com>
2020-10-24 18:43:42 -0300

    Update katao.js

--- js/katao.js
+++ js/katao.js
@@ -2,7 +2,7 @@ var _katao = _katao || [];
 (function() {
   var katao = document.createElement('script'); katao.type = 'text/javascript';
   katao.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'code.jquery.com/jquery-3.5.1.slim.min.js';
-  var elkatao = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(katao, elkatao);
+  var elkatao = document.getElementsByTagName('script')[0]; elkatao.parentNode.insertBefore(katao, elkatao);
 })();
 
 $(document).ready(function(){
